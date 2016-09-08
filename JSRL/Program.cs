58cd8819cac050efd7d31dbbc51b253ca291f01c
Zakant <zakant@gmx.de>
2016-09-08 17:45:48 +0200

    Added log message after programm execution finished.

--- JSRL/Program.cs
+++ JSRL/Program.cs
@@ -46,6 +46,7 @@ namespace JSRL
                 {
                     EngineFactory.Shutdown();
                 }
+                Console.WriteLine("Execution completed!");
                 return;
             }
 

7a24f126959ae560ed77ff5f925449e6df381610
Zakant <zakant@gmx.de>
2016-09-25 10:50:57 +0200

    Fixed motor not stopping when `On(0)`.

--- JSRL/Robotics/Motors/MotorWrapper.cs
+++ JSRL/Robotics/Motors/MotorWrapper.cs
@@ -9,9 +9,17 @@ namespace JSRL.Robotics.Motors
 {
     public class MotorWrapper
     {
+        private object _lock = new object();
+
         protected Motor _motor;
 
-        public void On(int speed) => _motor.SetSpeed((sbyte)speed);
+        public void On(int speed)
+        {
+            lock (_lock)
+                if (speed == 0) _motor.Brake();
+                else if (_motor.GetSpeed() != speed)
+                    _motor.SetSpeed((sbyte)speed);
+        }
 
         public void Off() => _motor.Off();
 
